c0d59ea8ba3f8c4a54167f4408d49974aa5c77c3
Yoda94 <philip20794@gmail.com>
2018-12-11 13:43:16 +0100

    doofer them button

--- Praxisaufgabe2_Tatiana_Nazarova_und_Philip_Spahn/js/theme.js
+++ Praxisaufgabe2_Tatiana_Nazarova_und_Philip_Spahn/js/theme.js
@@ -1,21 +1,27 @@
-var darkTheme = true; 
-
-function changeTheme() {
-	if (darkTheme){
+function changeTheme(styleDir) {
+	var theme = localStorage.getItem('theme') || 'dark';
+	if (theme === "dark"){
 		document.getElementById("themeBut").textContent = "Dark Theme";
-		document.getElementById("theme").href = "light.css";
-		darkTheme = false;
+		document.getElementById("theme").href = styleDir + "light-theme.css";
+		localStorage.setItem('theme', "light");
 	}else{
 		document.getElementById("themeBut").textContent = "Light Theme";
-		document.getElementById("theme").href = "dark.css";
-		darkTheme = true;
+		document.getElementById("theme").href = styleDir + "dark-theme.css";
+		localStorage.setItem('theme', "dark");
 	}
 }
 
-function updateTheme() {
-	if (darkTheme){
-		document.getElementById("theme").href = "dark.css";
+function updateTheme(styleDir) {
+	var theme = localStorage.getItem('theme') || 'dark';
+	if (theme === "dark"){
+		document.getElementById("themeBut").textContent = "Light Theme";
+		document.getElementById("theme").href = styleDir + "dark-theme.css";
 	}else{
-		document.getElementById("theme").href = "light.css";
+		document.getElementById("themeBut").textContent = "Dark Theme";
+		document.getElementById("theme").href = styleDir + "light-theme.css";
 	}
+}
+
+function test(styleDir){
+	document.getElementById("theme").href = styleDir + "dark-theme.css";
 }
